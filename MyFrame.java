2338c5a774bc540c13b1f6998feae7ada4af8093
Yin Lee <leesrrh@gmail.com>
2024-04-15 22:51:07 +0800

    basic layout

--- MyFrame.java
+++ MyFrame.java
@@ -1,3 +1,13 @@
-public class MyFrame extends  JFrame {
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
 
+public class MyFrame extends  JFrame {
+  MyFrame() {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setTitle("My Clock Program");
+    this.setLayout(new FlowLayout());
+    this.setSize(350,200);
+    this.setResizable(false);
+    this.setVisible(true);
+  }
 }
