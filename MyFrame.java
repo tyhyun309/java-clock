a47d6d52fb2fdf39485cdfddd656aa7e4bbe5505
Yin Lee <leesrrh@gmail.com>
2024-04-16 22:04:17 +0800

    added time stamp

--- MyFrame.java
+++ MyFrame.java
@@ -1,13 +1,30 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import java.awt.FlowLayout;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MyFrame extends  JFrame {
+
+  Calendar calendar;
+  SimpleDateFormat timeFormat;
+  JLabel timeLabel;
+  String time;
+
   MyFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setTitle("My Clock Program");
     this.setLayout(new FlowLayout());
     this.setSize(350,200);
     this.setResizable(false);
+
+    timeFormat = new SimpleDateFormat("hh:mm:ss a");
+    timeLabel = new JLabel();
+    time = timeFormat.format(Calendar.getInstance().getTime());
+    timeLabel.setText(time);
+
+    this.add(timeLabel);
     this.setVisible(true);
   }
 }
