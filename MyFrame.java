1739c67a677f9bd5768b4c6c33e14d0bb190589c
Yin Lee <leesrrh@gmail.com>
2024-04-18 23:08:19 +0800

    added day format and font style

--- MyFrame.java
+++ MyFrame.java
@@ -1,7 +1,9 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -9,8 +11,11 @@ public class MyFrame extends  JFrame {
 
   Calendar calendar;
   SimpleDateFormat timeFormat;
+  SimpleDateFormat dayFormat;
   JLabel timeLabel;
+  JLabel dayLabel;
   String time;
+  String day;
 
   MyFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,9 +25,23 @@ public class MyFrame extends  JFrame {
     this.setResizable(false);
 
     timeFormat = new SimpleDateFormat("hh:mm:ss a");
+    dayFormat = new SimpleDateFormat("EEEE");
+
+
     timeLabel = new JLabel();
+    timeLabel.setFont(new Font("Arial",Font.PLAIN,50));
+    timeLabel.setForeground(new Color(0,76,153));
+    timeLabel.setBackground(Color.BLACK);
+    timeLabel.setOpaque(true);
+
+    dayLabel = new JLabel();
+    dayLabel.setFont(new Font("Monospaced",Font.PLAIN,50));
+    dayLabel.setForeground(new Color(0,204,204));
+    dayLabel.setBackground(Color.BLACK);
+    dayLabel.setOpaque(true);
 
     this.add(timeLabel);
+    this.add(dayLabel);
     this.setVisible(true);
 
     setTime();
@@ -31,6 +50,9 @@ public class MyFrame extends  JFrame {
     while(true){
       time = timeFormat.format(Calendar.getInstance().getTime());
       timeLabel.setText(time);
+
+      day = dayFormat.format(Calendar.getInstance().getTime());
+      dayLabel.setText(day);
       try {
         Thread.sleep(1000);
 
